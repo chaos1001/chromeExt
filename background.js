6169bddda5b33c9f600df264bc51ee524fe7a33d
ccyang <ccyang@anjuke.com>
2013-06-25 14:02:21 +0800

    update

--- background.js
+++ background.js
@@ -7,23 +7,30 @@ function show() {
   var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
   var hour = time[1];               // The prettyprinted hour.
   var minute = time[2]; // The period of the day.
-  var notification = window.webkitNotifications.createNotification('kuroko.png','notification demo','Judgement.'+hour+' '+minute);
+  var notification = window.webkitNotifications.createNotification('kuroko.png','啊！！','是不是还有今天结束的task没有点掉..');
   notification.show();
+  
+  notification.addEventListener('click',function(){
+  	chrome.tabs.create({url: 'http://p.corp.anjuke.com/'});
+  	notification.cancel();
+  })
+
 }
 
 // Test for notification support.
 if (window.webkitNotifications) {  
-  var expectedHour='11';
-  var expectedMin='28';
-  var expectedTime=expectedHour+':'+expectedMin;
+
   var interval = 60; // The notification interval, in second.
   
   setInterval(function() {
+  	var expectedHour=localStorage["hour"];
+  	var expectedMin=localStorage["minute"];
+  	var expectedTime=expectedHour+':'+expectedMin;
+  	
   	var time = /(..)(:..)/.exec(new Date());
   	var thisTime = time[0];
   	
     if (thisTime.indexOf(expectedTime)>=0) {
-    	console.log(expectedTime);
       show();
     }
   }, interval*1000);
---
+++ options.js
@@ -0,0 +1,46 @@
+﻿// foolish way
+// Saves options to localStorage.
+function save_options() {
+  var hour = document.getElementById("hour");
+  localStorage["hour"] = hour.value;
+
+	var minute = document.getElementById("minute");
+  localStorage["minute"] = minute.value;
+  
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = "Options Saved.";
+  setTimeout(function() {status.innerHTML = "";}, 750);
+  
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+ 
+  var hour = document.getElementById("hour");
+  var minute = document.getElementById("minute");
+
+	hour.value = localStorage["hour"];
+  minute.value = localStorage["minute"];
+  
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.querySelector('#save').addEventListener('click', save_options);
+
+
+//another(cool) way and no button
+/*
+window.addEventListener('load', function() {
+
+  options.hour.value = localStorage.hour;
+  options.hour.onchange = function() {
+    localStorage.hour = options.hour.value;
+  }
+  
+  options.minute.value = localStorage.minute;
+  options.minute.onchange = function() {
+    localStorage.minute = options.minute.value;
+  }
+});
+*/
